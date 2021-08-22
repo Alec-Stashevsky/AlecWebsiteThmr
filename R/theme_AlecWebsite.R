82a1089c25f27ad3c909fa61286b2f163b979c15
Alec Stashevsky <alec.stashevsky@protonmail.com>
2021-08-22 13:18:50 -0700

    update README

--- R/theme_AlecWebsite.R
+++ R/theme_AlecWebsite.R
@@ -1,33 +1,32 @@
 #' Consistent Theme for Alec's Personal Website
 #'
-#' @inheritParams ggplot2::theme_grey
-#' @return
 #' @export
 theme_AlecWebsite <- function(){
 
-  theme_gray() %+replace%    #replace elements we want to change
+  '%+replace%' <- ggplot2::'%+replace%'
 
-    theme(
-      plot.margin = unit(c(0, 0, 0, 0), "cm"),
-      axis.title.x = element_text(margin = margin(10, 0, 0, 0)),
-      axis.title.y = element_text(margin = margin(0, 10, 0, 0)),
-      plot.title = element_text(size = 16),
+  ggplot2::theme_gray() %+replace%
+
+    ggplot2::theme(
+      plot.margin = ggplot2::unit(c(0.1, 0.2, 0.1, 0.1), "cm"),
+      axis.title.x = ggplot2::element_text(margin = ggplot2::margin(10, 0, 0, 0)),
+      axis.title.y = ggplot2::element_text(margin = ggplot2::margin(0, 10, 0, 0)),
+      plot.title = ggplot2::element_text(size = 16),
       legend.position = "bottom",
-      plot.background = element_rect(fill = "#fff8e3", color = "#fff8e3"),
-      legend.background = element_rect(fill = "#fff8e3"),
-      strip.background = element_rect(fill = "#e8d8c3"),
-      panel.grid = element_line(color = "#e8d8c3"),
-      panel.background = element_blank(),
-      axis.ticks = element_line(color = "#e8d8c3"),
-      plot.caption = element_text(hjust = -0.1),
-      text = element_text(color = "#33322e")
+      plot.background = ggplot2::element_rect(fill = "#fff8e3", color = "#fff8e3"),
+      legend.background = ggplot2::element_rect(fill = "#fff8e3"),
+      strip.background = ggplot2::element_rect(fill = "#e8d8c3"),
+      panel.grid = ggplot2::element_line(color = "#e8d8c3"),
+      panel.background = ggplot2::element_blank(),
+      axis.ticks = ggplot2::element_line(color = "#e8d8c3"),
+      plot.caption = ggplot2::element_text(hjust = -0.1),
+      text = ggplot2::element_text(color = "#33322e")
     )
 }
 
 
 #' Primary plotting color for Alec's Personal Website
 #'
-#' @return
 #' @export
 #'
 get_primary_color <- function() {
---
+++ README.md
@@ -0,0 +1,5 @@
+
+# AlecWebsiteThmr
+
+Custom ggplot2 theme with consistent styling of my [personal
+website](https://alecstashevsky.com).
